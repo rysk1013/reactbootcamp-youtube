09bbe6c2298c7001fee7308715208b5c390d3e9b
rysk1013 <rysk2908@gmail.com>
2021-10-19 14:39:11 +0900

    add:サイドバー作成

--- src/index.tsx
+++ src/index.tsx
@@ -2,26 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { RootRouter } from "./Route";
-
-// Material-UIの設定類をインポート
 import { createTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
 
-// Material-UIの「テーマ」を作成する。
-// Material-UIのテーマ（色など）をカスタマイズする際には、createThemeの引数にカスタマイズ項目を渡す。
-// 今回は何もカスタマイズしないので、何も指定していない。
+// 先程のグローバルスタイルをimport
+import GlobalStyle from "./GlobalStyle";
+
+
 const theme = createTheme();
 
 ReactDOM.render(
   <React.StrictMode>
-    {/*
-      Material-UI用を初期化し、アプリケーション全体でMaterial-UIを使用できるようにする
-    */}
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        {/*
-          ブラウザの違いを吸収し、どのデバイスでも同じように表示する用のCSSを使用する
-        */}
         <CssBaseline />
+
+        {/*
+          アプリ全体の特殊なグローバルスタイリング
+        */}
+        <GlobalStyle />
+
         <RootRouter />
       </BrowserRouter>
     </ThemeProvider>
--- src/layouts/Home/index.tsx
+++ src/layouts/Home/index.tsx
@@ -1,17 +1,28 @@
 import { Outlet } from "react-router-dom";
-
-// DashboardHeaderをimport
 import { DashboardHeader } from "../../templates/DashboardHeader";
+import { Sidebar } from "../../templates/Sidebar";
+import useStyles from "./style";
 
 export const HomeLayout = () => {
+  const styles = useStyles();
   return (
-    <div>
-      {/*
-        DashboardHeaderコンポーネントを表示する
-      */}
+
+    // 一番上位の<div>に対して"root"スタイリングを指定
+    // 今ままで`<div className={styles.flex}>`としていた要素は削除
+    <div className={styles.root}>
+
       <DashboardHeader />
 
-      <Outlet />
+      {/*
+        `<div className={styles.flex}>`としていた要素は削除
+      */}
+      <div className={styles.sidebar}>
+        <Sidebar />
+      </div>
+
+      <div className={styles.main}>
+        <Outlet />
+      </div>
     </div>
   );
 };
