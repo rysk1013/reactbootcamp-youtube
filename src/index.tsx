e53427ace2d1228c1daeaa70c45154f339315e29
rysk1013 <rysk2908@gmail.com>
2021-10-19 12:07:10 +0900

    add:ルーティングの作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { RootRouter } from "./Route";
+
+// Material-UIの設定類をインポート
+import { createTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
+
+// Material-UIの「テーマ」を作成する。
+// Material-UIのテーマ（色など）をカスタマイズする際には、createThemeの引数にカスタマイズ項目を渡す。
+// 今回は何もカスタマイズしないので、何も指定していない。
+const theme = createTheme();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*
+      Material-UI用を初期化し、アプリケーション全体でMaterial-UIを使用できるようにする
+    */}
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        {/*
+          ブラウザの違いを吸収し、どのデバイスでも同じように表示する用のCSSを使用する
+        */}
+        <CssBaseline />
+        <RootRouter />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
